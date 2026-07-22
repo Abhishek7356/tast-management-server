270b2f1118ddd870f567448dd6d1b0729c3bbbb9
Abhishek7356 <ksabhishek32@gmail.com>
2026-07-22 15:23:04 +0530

    complete  jwt login authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { registeUser } from "../services/auth.service.js";
+import { loginUser, registeUser } from "../services/auth.service.js";
 
 
 export const register = async (req, res) => {
@@ -8,6 +8,24 @@ export const register = async (req, res) => {
         res.status(201).json({
             success: true,
             message: "User registered successfully",
+            user: result
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const result = await loginUser(req.body)
+
+        res.status(201).json({
+            success: true,
+            message: "User logged in successfully",
             user: result
         });
 
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,6 @@
 import { createUser, findUserByEmail } from "../models/User.js";
 import bcrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
 
 
 export const registeUser = async (data) => {
@@ -20,6 +21,37 @@ export const registeUser = async (data) => {
             name,
             email,
             role: role || "user"
+        };
+    } catch (error) {
+        console.error(error)
+        throw new Error(error.message)
+    }
+}
+
+export const loginUser = async (data) => {
+    try {
+        const { email, password } = data;
+        const existingUser = await findUserByEmail(email)
+
+        if (!existingUser) {
+            throw new Error("Invalid email or password");
+        }
+
+        const isMatch = await bcrypt.compare(password, existingUser.password);
+
+        if (!isMatch) {
+            throw new Error("Invalid email or password");
+        }
+
+        const token = generateToken(existingUser.id)
+
+
+        return {
+            id: existingUser.id,
+            name: existingUser.name,
+            email: existingUser.email,
+            role: existingUser.role,
+            token
         };
     } catch (error) {
         console.error(error)
